b1902ec6be312ce7ea9c89733ea2bb04f9db99b5
Mark Dellacca <markad88@hotmail.com>
2025-12-02 20:43:18 -0800

    Day 1 & 2

---
+++ 01_secret_entrance/Program.cs
@@ -0,0 +1,50 @@
+﻿var input = File.ReadLines("input.txt");
+// input = File.ReadLines("example.txt");
+
+bool pointerStartedAtZero = false;
+int zeroClicks = 0;
+int zeroCount = 0;
+int pointer = 50;
+foreach (var line in input)
+{
+    var direction = line[0] == 'L' ? -1 : 1;
+    var distance = int.Parse(line[1..]);
+    pointer += (direction * distance);
+
+    // Part 1
+    if (pointer % 100 == 0)
+    {
+        zeroCount++;
+    }
+
+    // Part 2
+    {
+        // Calculate times passed zero
+        zeroClicks += Math.Abs(pointer) / 100;
+
+        // Handle negative cross overs
+        // Handle exact zero
+        if ((pointer < 0 && !pointerStartedAtZero) || pointer == 0)
+        {
+            zeroClicks++;
+        }
+
+        // Move pointer in bounds
+        pointer %= 100;
+
+        // Reset flag
+        pointerStartedAtZero = pointer == 0;
+
+        // Handle wrapping
+        if (pointer < 0)
+        {
+            pointer += 100;
+        }
+    }
+
+    Console.WriteLine($"{line[0]} {distance,4} {pointer,3} {zeroCount,4} {zeroClicks,4}");
+}
+
+Console.WriteLine();
+Console.WriteLine($"ZeroCount: {zeroCount}");
+Console.WriteLine($"ZeroClick: {zeroClicks}");
---
+++ 02_gift_shop/Program.cs
@@ -0,0 +1,92 @@
+﻿var input = File.ReadAllText("input.txt").Split(',');
+// input = File.ReadAllText("example.txt").Split(',');
+//input = ["998-1012"];
+
+List<long> invalidIds = [];
+foreach (var line in input)
+{
+    var parts = line.Split('-').Select(long.Parse).ToArray();
+    var start = parts[0];
+    var end = parts[1];
+
+    // Part 1
+    var invalid = CreateLongRange(start, end)
+        .Select(i => i.ToString())
+        .Where(id => id.Length % 2 == 0)
+        .Where(id => id[..(id.Length / 2)] == id[(id.Length / 2)..])
+        .Select(long.Parse)
+        .ToList();
+
+    // Part 2
+    foreach (var id in CreateLongRange(start, end).Except(invalid).Select(id => id.ToString()).Where(i => i.Length > 1))
+    {
+        // Entire number is made up of same digit
+        if (id.All(x => x == id[0]))
+        {
+            invalid.Add(long.Parse(id));
+            continue;
+        }
+
+        // Check for recurring duplication
+        for (int length = 2; length <= Math.Ceiling((double) id.Length / 2); length++)
+        {
+            var part = id[..length];
+            var occurances = id.Split(part, StringSplitOptions.RemoveEmptyEntries);
+            if (occurances.Length == 0)
+            {
+                invalid.Add(long.Parse(id));
+            }
+        }
+    }
+
+    invalidIds.AddRange(invalid);
+    Console.WriteLine($"Range {start}-{end}: Found {(invalid.Count > 0 ? string.Join(", ", invalid) : "NO")} invalid IDs");
+}
+
+Console.WriteLine(invalidIds.Sum());
+
+static IEnumerable<long> CreateLongRange(long start, long end)
+{
+    while (start <= end)
+    {
+        yield return start;
+        start++;
+    }
+}
+
+/*
+    var rowPart = line[..7];
+    var colPart = line[7..];
+    int rowMin = 0;
+    int rowMax = 127;
+    foreach (var c in rowPart)
+    {
+        int mid = (rowMin + rowMax) / 2;
+        if (c == 'F')
+        {
+            rowMax = mid;
+        }
+        else
+        {
+            rowMin = mid + 1;
+        }
+    }
+    int row = rowMin;
+    int colMin = 0;
+    int colMax = 7;
+    foreach (var c in colPart)
+    {
+        int mid = (colMin + colMax) / 2;
+        if (c == 'L')
+        {
+            colMax = mid;
+        }
+        else
+        {
+            colMin = mid + 1;
+        }
+    }
+    int col = colMin;
+    int seatId = (row * 8) + col;
+    invalidIds.Add(seatId);
+*/
